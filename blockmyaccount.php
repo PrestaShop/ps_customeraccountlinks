fcc3bb5008276bd3fdbcff91bcca14e18ca9730b
Maxime Biloé <maxime.biloe@prestashop.com>
2015-10-05 18:05:25 +0200

    //Ready for StarterTheme

--- blockmyaccount.php
+++ blockmyaccount.php
@@ -24,91 +24,118 @@
 *  International Registered Trademark & Property of PrestaShop SA
 */
 
-if (!defined('_PS_VERSION_'))
-	exit;
-
-class BlockMyAccount extends Module
-{
-	public function __construct()
-	{
-		$this->name = 'blockmyaccount';
-		$this->tab = 'front_office_features';
-		$this->version = '1.4.0';
-		$this->author = 'PrestaShop';
-		$this->need_instance = 0;
-
-		parent::__construct();
-
-		$this->displayName = $this->l('My Account block');
-		$this->description = $this->l('Displays a block with links relative to a user\'s account.');
-		$this->ps_versions_compliancy = array('min' => '1.6', 'max' => _PS_VERSION_);
-	}
-
-	public function install()
-	{
-		if (!$this->addMyAccountBlockHook() 
-			|| !parent::install() 
-			|| !$this->registerHook('displayLeftColumn') 
-			|| !$this->registerHook('displayHeader')
-			|| !$this->registerHook('actionModuleRegisterHookAfter')
-			|| !$this->registerHook('actionModuleUnRegisterHookAfter'))
-			return false;
-		return true;
-	}
-
-	public function uninstall()
-	{
-		return (parent::uninstall() && $this->removeMyAccountBlockHook());
-	}
-
-	public function hookActionModuleUnRegisterHookAfter($params)
-	{
-		return $this->hookActionModuleRegisterHookAfter($params);
-	}
-
-	public function hookActionModuleRegisterHookAfter($params)
-	{
-		if ($params['hook_name'] == 'displayMyAccountBlock')
-			$this->_clearCache('blockmyaccount.tpl');
-	}
-
-	public function hookDisplayLeftColumn($params)
-	{
-		if (!$this->context->customer->isLogged())
-			return false;
-
-		$this->smarty->assign(array(
-			'voucherAllowed' => CartRule::isFeatureActive(),
-			'returnAllowed' => (int)Configuration::get('PS_ORDER_RETURN'),
-			'HOOK_BLOCK_MY_ACCOUNT' => Hook::exec('displayMyAccountBlock'),
-		));
-		return $this->display(__FILE__, $this->name.'.tpl');
-	}
-
-	public function hookDisplayRightColumn($params)
-	{
-		return $this->hookDisplayLeftColumn($params);
-	}
-	
-	public function hookDisplayFooter($params)
-	{
-		return $this->hookDisplayLeftColumn($params);
-	}
-
-	private function addMyAccountBlockHook()
-	{
-		return Db::getInstance()->execute('INSERT IGNORE INTO `'._DB_PREFIX_.'hook` (`name`, `title`, `description`, `position`) VALUES (\'displayMyAccountBlock\', \'My account block\', \'Display extra informations inside the "my account" block\', 1)');
-	}
-
-	private function removeMyAccountBlockHook()
-	{
-		return Db::getInstance()->execute('DELETE FROM `'._DB_PREFIX_.'hook` WHERE `name` = \'displayMyAccountBlock\'');
-	}
-
-	public function hookDisplayHeader($params)
-	{
-		$this->context->controller->addCSS(($this->_path).'blockmyaccount.css', 'all');
-	}
+if (!defined('_PS_VERSION_')) {
+    exit;
 }
 
+use PrestaShop\PrestaShop\Core\Business\Module\WidgetInterface;
 
+class BlockMyAccount extends Module implements WidgetInterface
+{
+    public function __construct()
+    {
+        $this->name = 'blockmyaccount';
+        $this->tab = 'front_office_features';
+        $this->version = '2.0';
+        $this->author = 'PrestaShop';
+        $this->need_instance = 0;
+
+        parent::__construct();
+
+        $this->displayName = $this->l('My Account block');
+        $this->description = $this->l('Displays a block with links relative to a user\'s account.');
+        $this->ps_versions_compliancy = array('min' => '1.7', 'max' => _PS_VERSION_);
+    }
+
+    public function install()
+    {
+        if (!parent::install()
+            || !$this->registerHook('displayFooter')
+            || !$this->registerHook('actionModuleRegisterHookAfter')
+            || !$this->registerHook('actionModuleUnRegisterHookAfter')) {
+            return false;
+        }
+        return true;
+    }
+
+    public function uninstall()
+    {
+        return (parent::uninstall() && $this->removeMyAccountBlockHook());
+    }
+
+    public function renderWidget($hookName = null, array $configuration = [])
+    {
+        if (!$this->context->customer->isLogged()) {
+            return false;
+        }
+
+        if (!$this->isCached($this->name.'.tpl', $this->getCacheId())) {
+            $this->smarty->assign($this->getWidgetVariables($hookName, $configuration));
+        }
+        return $this->display(__FILE__, $this->name.'.tpl', $this->getCacheId());
+    }
+
+    public function getWidgetVariables($hookName = null, array $configuration = [])
+    {
+        $link = $this->context->link;
+
+        $my_account_urls = [
+            0 => [
+                'title' => $this->l('My orders'),
+                'url' => $link->getPageLink('history', true),
+            ],
+            2 => [
+                'title' => $this->l('My credit slips'),
+                'url' => $link->getPageLink('order-slip', true),
+            ],
+            3 => [
+                'title' => $this->l('My addresses'),
+                'url' => $link->getPageLink('addresses', true),
+            ],
+            4 => [
+                'title' => $this->l('My personal info'),
+                'url' => $link->getPageLink('identity', true),
+            ],
+        ];
+
+        if ((int)Configuration::get('PS_ORDER_RETURN')) {
+            $my_account_urls[1] = [
+                'title' => $this->l('My merchandise returns'),
+                'url' => $link->getPageLink('order-follow', true),
+            ];
+        }
+
+        if (CartRule::isFeatureActive()) {
+            $my_account_urls[5] = [
+                'title' => $this->l('My vouchers'),
+                'url' => $link->getPageLink('discount', true),
+            ];
+        }
+
+        sort($my_account_urls);
+
+        return [
+            'my_account_urls' => $my_account_urls,
+        ];
+    }
+
+    public function hookActionModuleUnRegisterHookAfter($params)
+    {
+        return $this->hookActionModuleRegisterHookAfter($params);
+    }
+    public function hookActionModuleRegisterHookAfter($params)
+    {
+        if ($params['hook_name'] == 'displayMyAccountBlock') {
+            $this->_clearCache($this->name.'.tpl');
+        }
+    }
+
+    private function addMyAccountBlockHook()
+    {
+        return Db::getInstance()->execute('INSERT IGNORE INTO `'._DB_PREFIX_.'hook` (`name`, `title`, `description`, `position`) VALUES (\'displayMyAccountBlock\', \'My account block\', \'Display extra informations inside the "my account" block\', 1)');
+    }
+    private function removeMyAccountBlockHook()
+    {
+        return Db::getInstance()->execute('DELETE FROM `'._DB_PREFIX_.'hook` WHERE `name` = \'displayMyAccountBlock\'');
+    }
+}
