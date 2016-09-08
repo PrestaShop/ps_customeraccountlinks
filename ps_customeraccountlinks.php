d48a139e73e59210a34d3deb27660575234a0896
Maxime Biloé <maxime.biloe@prestashop.com>
2016-09-08 15:14:27 +0200

    // Version updated to v1.0.1

--- ps_customeraccountlinks.php
+++ ps_customeraccountlinks.php
@@ -36,7 +36,7 @@ class Ps_Customeraccountlinks extends Module implements WidgetInterface
     {
         $this->name = 'ps_customeraccountlinks';
         $this->tab = 'front_office_features';
-        $this->version = '1.0.0';
+        $this->version = '1.0.1';
         $this->author = 'PrestaShop';
         $this->need_instance = 0;
 
