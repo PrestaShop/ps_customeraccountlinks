32615ddea2d992343486ecc69be75cb01f24f5f7
MathiasReker <mathias@reker.dk>
2018-12-21 02:19:05 +0100

    Cast spaces

--- ps_customeraccountlinks.php
+++ ps_customeraccountlinks.php
@@ -116,7 +116,7 @@ class Ps_Customeraccountlinks extends Module implements WidgetInterface
             ],
         ];
 
-        if ((int)Configuration::get('PS_ORDER_RETURN')) {
+        if ((int) Configuration::get('PS_ORDER_RETURN')) {
             $my_account_urls[1] = [
                 'title' => $this->trans('Merchandise returns', [], 'Modules.Customeraccountlinks.Admin'),
                 'url' => $link->getPageLink('order-follow', true),
