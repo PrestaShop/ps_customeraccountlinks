85da47d9a552534ed588533ef6c7236f989fe423
Thomas Nabord <thomas.nabord@prestashop.com>
2019-06-28 14:50:51 +0100

    Uninstall module used on PS 1.6 before using this one

--- ps_customeraccountlinks.php
+++ ps_customeraccountlinks.php
@@ -32,6 +32,11 @@ use PrestaShop\PrestaShop\Core\Module\WidgetInterface;
 
 class Ps_Customeraccountlinks extends Module implements WidgetInterface
 {
+    /**
+     * @var string Name of the module running on PS 1.6.x. Used for data migration.
+     */
+    const PS_16_EQUIVALENT_MODULE = 'blockmyaccount';
+
     private $templateFile;
 
     public function __construct()
@@ -53,6 +58,7 @@ class Ps_Customeraccountlinks extends Module implements WidgetInterface
 
     public function install()
     {
+        $this->uninstallPrestaShop16Module();
         return parent::install()
             && $this->registerHook('actionModuleRegisterHookAfter')
             && $this->registerHook('actionModuleUnRegisterHookAfter')
@@ -63,6 +69,27 @@ class Ps_Customeraccountlinks extends Module implements WidgetInterface
     public function uninstall()
     {
         return parent::uninstall();
+    }
+
+    /**
+     * Migrate data from 1.6 equivalent module (if applicable), then uninstall
+     */
+    public function uninstallPrestaShop16Module()
+    {
+        if (!Module::isInstalled(self::PS_16_EQUIVALENT_MODULE)) {
+            return false;
+        }
+        $oldModule = Module::getInstanceByName(self::PS_16_EQUIVALENT_MODULE);
+        if ($oldModule) {
+            // This closure calls the parent class to prevent data to be erased
+            // It allows the new module to be configured without migration
+            $parentUninstallClosure = function() {
+                return parent::uninstall();
+            };
+            $parentUninstallClosure = $parentUninstallClosure->bindTo($oldModule, get_class($oldModule));
+            $parentUninstallClosure();
+        }
+        return true;
     }
 
     public function hookActionModuleUnRegisterHookAfter($params)
